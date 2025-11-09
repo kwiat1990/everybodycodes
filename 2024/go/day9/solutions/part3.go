93446e46e2f9a93cce3a196d8c1799a63be491b9
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2025-11-09 01:33:10 +0100

    add day 9

--- 2024/go/day9/solutions/part3.go
+++ 2024/go/day9/solutions/part3.go
@@ -19,6 +19,8 @@ func Part3(input string) int {
 	coins := [...]int{1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101}
 	sum := 0
 
+	slices.Sort(amounts)
+
 	for _, amount := range amounts {
 		dp := slices.Repeat([]int{math.MaxInt}, amount+1)
 		dp[0] = 0
@@ -32,7 +34,17 @@ func Part3(input string) int {
 				dp[i] = min(dp[i], 1+dp[diff])
 			}
 		}
-		sum += dp[amount]
+
+		minAmount := math.MaxInt
+		lo := amount / 2
+		hi := amount / 2
+		if amount%2 == 1 {
+			hi += 1
+		}
+		for i := range 50 {
+			minAmount = min(minAmount, dp[lo+i]+dp[hi-i])
+		}
+		sum += minAmount
 	}
 
 	return sum
