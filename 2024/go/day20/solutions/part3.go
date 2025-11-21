e65ea82915a7a28a1490cfb75bbf6ad2e371869b
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2025-11-21 09:18:30 +0100

    add day 20

---
+++ 2024/go/day20/solutions/part3.go
@@ -0,0 +1,5 @@
+package solutions
+
+func Part3(input string) int {
+	return 0
+}
---
+++ 2024/go/day20/solutions/solutions_test.go
@@ -0,0 +1,38 @@
+package solutions
+
+import (
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := `#....S....#
+#.........#
+#---------#
+#.........#
+#..+.+.+..#
+#.+-.+.++.#
+#.........#`
+	want := 1045
+
+	if got := Part1(input); got != want {
+		t.Fatalf("got = %d, want = %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := ``
+	want := 0
+
+	if got := Part2(input); got != want {
+		t.Fatalf("got = %d, want = %d", got, want)
+	}
+}
+
+func TestPart3(t *testing.T) {
+	input := ``
+	want := 0
+
+	if got := Part3(input); got != want {
+		t.Fatalf("got = %d, want = %d", got, want)
+	}
+}
